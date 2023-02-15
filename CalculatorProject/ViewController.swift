2441a559a18d21bb989baf9904eecf10b511828a
Alejandro Aibar <alexaib@outlook.es>
2023-02-15 13:04:19 +0100

    Finished basic requirements

--- CalculatorProject/ViewController.swift
+++ CalculatorProject/ViewController.swift
@@ -2,8 +2,6 @@
 //  ViewController.swift
 //  CalculatorProject
 //
-//  Created by Usuario invitado on 8/2/23.
-//
 
 import UIKit
 
@@ -14,55 +12,74 @@ let operations: Dictionary = [
     "Divide": div,
     "Module": mod,
 ]
-var selOperation: (Int, Int) -> Int = operations["Sum"]!
+var selOperation: (Double, Double) -> Double = operations["Sum"]!
 
 class ViewController: UIViewController {
     @IBOutlet weak var resultLbl: UILabel!
-    var fRes = 0
-    var lRes = 0
+    var res = 0.0
+    var mustRepaint = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func setOperation(_ sender: Any) {
-        if sender is UIButton {
-            let senderBtn: UIButton = sender as! UIButton
-            print(senderBtn.accessibilityHint!)
-            selOperation = operations[senderBtn.accessibilityHint!]!
-        }
+    @IBAction func setOperation(_ sender: UIButton) {
+        selOperation = operations[sender.accessibilityHint!]!
+        res = Double(resultLbl.text!)!
+        mustRepaint = true
     }
 
-    @IBAction func onNbrButtonPressed(_ sender: Any) {
-        
+    @IBAction func onNbrButtonPressed(_ sender: UIButton) {
+        if mustRepaint {
+            resultLbl.text = ""
+            mustRepaint = false
+        }
+        resultLbl.text = resultLbl.text! + sender.titleLabel!.text!
+    }
+    
+    @IBAction func onClearButtonPressed(_ sender: Any) {
+        resultLbl.text = "0"
+        clearCaches()
+    }
+    
+    @IBAction func onPeriodPressed() {
+        resultLbl.text = resultLbl.text! + "."
     }
     
     @IBAction func onEnterButtonPressed(_ sender: Any) {
         resultLbl.text = String(startCalculation())
+        clearCaches()
+        if resultLbl.text == "inf" || resultLbl.text == "nan" {
+            resultLbl.text = "Error"
+        }
     }
     
-    func startCalculation() -> Int {
-        return selOperation(fRes, lRes)
+    func startCalculation() -> Double {
+        return selOperation(res, Double(resultLbl.text!)!)
+    }
+    
+    func clearCaches() {
+        res = 0
+        mustRepaint = true
     }
 }
 
-func sum(a: Int, b: Int) -> Int {
+func sum(a: Double, b: Double) -> Double {
     return a + b
 }
 
-func sub(a: Int, b: Int) -> Int {
+func sub(a: Double, b: Double) -> Double {
     return a - b
 }
 
-func mult(a: Int, b: Int) -> Int {
+func mult(a: Double, b: Double) -> Double {
     return a * b
 }
 
-func div(a: Int, b: Int) -> Int {
+func div(a: Double, b: Double) -> Double {
     return a / b
 }
 
-func mod(a: Int, b: Int) -> Int {
-    return a % b
+func mod(a: Double, b: Double) -> Double {
+    return a.truncatingRemainder(dividingBy: b)
 }
-
